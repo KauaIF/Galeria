010f7cea7a6899ca586744faaf276c22cb1b493e
kaua <kaua271203@gmail.com>
2023-07-07 08:56:10 -0300

    FIM?

--- app/src/main/java/mateus/kaua/galeria/activity/PhotoActivity.java
+++ app/src/main/java/mateus/kaua/galeria/activity/PhotoActivity.java
@@ -4,18 +4,26 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+
+import java.io.File;
 
 import mateus.kaua.galeria.R;
 
 public class PhotoActivity extends AppCompatActivity {
 
+    String photoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +34,13 @@ public class PhotoActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Intent i = getIntent();
+        photoPath = i.getStringExtra("photo_path");
+
+        Bitmap bitmap = mateus.kaua.applista.activity.util.Util.getBitmap(photoPath);
+        ImageView imPhoto = findViewById(R.id.imPhoto);
+        imPhoto.setImageBitmap(bitmap);
     }
 
     @Override
@@ -45,6 +60,12 @@ public class PhotoActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
-
+    void sharePhoto(){
+        //Codigo para compartilhar a foto
+        Uri photoUri = FileProvider.getUriForFile(PhotoActivity.this,"mateus.kaua.galeria.fileprovider",new File(photoPath));
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.putExtra(Intent.EXTRA_STREAM,photoUri);
+        i.setType("image/jpeg");
+        startActivity(i);
+    }
 }
--- app/src/main/java/mateus/kaua/galeria/adapter/MainAdapter.java
+++ app/src/main/java/mateus/kaua/galeria/adapter/MainAdapter.java
@@ -2,6 +2,7 @@ package mateus.kaua.galeria.adapter;
 
 import android.graphics.Bitmap;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -18,22 +19,34 @@ public class MainAdapter extends RecyclerView.Adapter {
 
         public MainAdapter(MainActivity mainActivity, List<String> photos) {
                 this.mainActivity = mainActivity;
+                this.photos = photos;
         }
-        this.photos = photos;
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+        }
 
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+
                 ImageView imPhoto = holder.itemView.findViewById(R.id.imPhoto);
                 int w = (int)mainActivity.getResources().getDimension(R.dimen.itemWidth);
                 int h = (int)mainActivity.getResources().getDimension(R.dimen.itemHeight);
-                Bitmap bitmap = Utils.getBitmap(photos.get(position)),w,h);
+                Bitmap bitmap = mateus.kaua.applista.activity.util.Util.getBitmap(photos.get(position),w,h);
                 imPhoto.setImageBitmap(bitmap);
                 imPhoto.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                                mainActivity.startActivity(photos.get(position));
+                                mainActivity.startPhotoActivity(photos.get(position));
                         }
                 });
+        }
+
+        @Override
+        public int getItemCount() {
+                return 0;
         }
 }
 
